125f4ad417a30fde92128f5d05d7c5a4e16c15c4
bynect <68197565+bynect@users.noreply.github.com>
2021-03-22 15:13:33 +0100

    Add test python syntax

--- src/syntax.c
+++ src/syntax.c
@@ -25,9 +25,33 @@ static struct SHD c_cpp_syntax = {
 };
 
 /*
+Python syntax highlighting descriptor
+*/
+
+static struct KWD python_kwd[] = {
+    {"if", 0x10}, {"else", 0x10}, {"elif", 0x10}, {"while", 0x10},
+    {"for", 0x10}, {"in", 0x10}, {"with", 0x10}, {"class", 0x10},
+    {"None", 0x20}, {"False", 0x20}, {"True", 0x20}, {"return", 0x20},
+    {"*", 0x30}, {";", 0x30}, {",", 0x30}
+};
+
+static const char *python_exts[] = {"py"};
+
+static struct SHD python_syntax = {
+    sizeof python_exts / sizeof *python_exts, python_exts,
+    " \t~!@#$%^&*()-=+[{]}\\|;:'\",.<>/?", // Characters that separates words
+    sizeof python_kwd / sizeof *python_kwd, python_kwd, //Keywords
+    0x40, 0x50, 0x05,
+    "\"\'`", // Strings charaters
+    "#", {"\"\"\"", "\"\"\""}, // Comments
+    {"{[(", ")]}"}
+};
+
+
+/*
 Global syntaxes
 */
-struct SHD *syntaxes[] = {&c_cpp_syntax};
+struct SHD *syntaxes[] = {&c_cpp_syntax, &python_syntax};
 
 void register_syntax(void) {
     config.syntaxes = syntaxes;
